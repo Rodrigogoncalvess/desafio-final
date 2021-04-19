a00bef4be4a34a6128a61840bd0ec12ed4c8b8e4
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-04-19 18:20:49 -0300

    Otimizando endpoints

--- src/main/java/com/rodrigo/desafiojava/controller/UserController.java
+++ src/main/java/com/rodrigo/desafiojava/controller/UserController.java
@@ -6,6 +6,7 @@ import com.rodrigo.desafiojava.domain.JwtRequest;
 import com.rodrigo.desafiojava.domain.Phones;
 import com.rodrigo.desafiojava.domain.User;
 import com.rodrigo.desafiojava.exceptionhandler.ReplyMessage;
+import com.rodrigo.desafiojava.repository.PhonesRepository;
 import com.rodrigo.desafiojava.repository.UserRepository;
 import com.rodrigo.desafiojava.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.*;
 
 @RestController
@@ -34,14 +36,16 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-
     @PostMapping("/create")
-    private ResponseEntity<?> create(@Valid @RequestBody User user) {
+    private ResponseEntity<?> create(@Valid @RequestBody User user,Phones phones){
 
         Optional<User> userExistsByEmail = userRepository.findByEmail(user.getEmail());
 
         if (!userExistsByEmail.isPresent()) {
             user.encryptPassword();
+            user.setCreated(LocalDateTime.now());
+            user.setModified(LocalDateTime.now());
+            user.setLast_login(LocalDateTime.now());
             return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(user));
         }
 
@@ -57,8 +61,8 @@ public class UserController {
             if (new BCryptPasswordEncoder().matches(user.getPassword(), userExistsByEmail.get().getPassword())) {
                 final String token = jwtUtility.generateToken(userExistsByEmail.get());
                 userExistsByEmail.get().setToken(token);
-                userRepository.save(userExistsByEmail.get());
-                return new ResponseEntity<User>(userExistsByEmail.get(), HttpStatus.OK);
+                userExistsByEmail.get().setLast_login(LocalDateTime.now());
+                return new ResponseEntity<User>( userRepository.save(userExistsByEmail.get()), HttpStatus.OK);
             }
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ReplyMessage("Usuário e/ou senha inválidos"));
         }
@@ -72,20 +76,16 @@ public class UserController {
     }
 
     @PutMapping("/update")
-    private ResponseEntity<?> updateUser(@Valid @RequestBody User user, Phones phones) throws Exception {
-        Optional<User> updateUser = userRepository.findByToken(user.getToken());
+    private ResponseEntity<?> updateUser(@Valid @RequestBody User user) throws Exception {
+
 
         if (!userRepository.existsById(user.getUuid())) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ReplyMessage("UUID não existe"));
 
         }
-        phones.setNumber(phones.getNumber());
-        phones.setDdd(phones.getDdd());
-        user.setModified(new Date());
-
+        user.setModified(LocalDateTime.now());
         userRepository.save(user);
-
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ReplyMessage("Atualizado com sucesso"));
     }
 
 
@@ -93,10 +93,10 @@ public class UserController {
     @DeleteMapping("/{uuid}")
     private ResponseEntity<?> delete(@PathVariable("uuid") UUID uuid) {
         if (!userRepository.existsById(uuid)) {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ReplyMessage("Usuário não existe"));
         }
         userRepository.deleteById(uuid);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ReplyMessage("Usuário excluído com sucesso"));
 
 
     }

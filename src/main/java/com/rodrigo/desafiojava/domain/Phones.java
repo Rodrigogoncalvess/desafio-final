7af63261669354cbf82f801275d399cb03099342
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-04-19 18:18:08 -0300

    Implementação da classe Phones

--- src/main/java/com/rodrigo/desafiojava/domain/Phones.java
+++ src/main/java/com/rodrigo/desafiojava/domain/Phones.java
@@ -4,6 +4,7 @@ package com.rodrigo.desafiojava.domain;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
@@ -14,7 +15,9 @@ public class Phones {
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size( min = 2 , max=3)
     private String ddd;
+    @Size(min = 8, max=9)
     private String number;
 
 
